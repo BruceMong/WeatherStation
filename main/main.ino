cc069f3fd76a7089b53002d3ba98be21280f5795
KazeoYa <58480180+KazeoYa@users.noreply.github.com>
2020-10-28 21:58:51 +0100

    bb

--- main/main.ino
+++ main/main.ino
@@ -6,7 +6,7 @@
 // je déclare les libs dans le .c et non dans le .h car sinon bug du makefile jcpa pq 
 #include <SoftwareSerial.h> //UART GPS 
 #include <Wire.h> //communication led 
-#include "DS1307.h" // clock RTC //bizarre 
+#include <DS1307.h> // clock RTC //bizarre 
 #include <SPI.h> //pour communication SPI ( carte SD) 
 #include <SD.h> //pour la carte SD 
 #include <avr/pgmspace.h> //PROGMEM library
